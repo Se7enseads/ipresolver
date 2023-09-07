00f148473ea2b3b8bcee990f7f36105bab6c01b6
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-08 00:27:36 +0300

    feat: add resolve_ip function

--- python/main.py
+++ python/main.py
@@ -1,8 +1,13 @@
+import socket
+import re
+import click
+import inquirer
+
+from urllib.parse import urlparse
+
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-from tabulate import tabulate
 
 Base = declarative_base()
 engine = create_engine('sqlite:///ip_addresses.db')
@@ -40,3 +45,37 @@ def get_ip_addresses():
 
     session.close()
     return ip_addresses
+
+
+def resolve_ip():
+    while True:
+        input_url = click.prompt(
+            "Please enter a website address (URL) or type 'back' to quit", default='', show_default=False)
+
+        if input_url == 'back':
+            print("Operation aborted by the user.")
+            break
+
+        try:
+            parsed_url = urlparse(input_url)
+            if parsed_url.netloc:
+                hostname = parsed_url.netloc
+            else:
+                hostname = input_url
+
+            if not is_valid_hostname(hostname):
+                print("Invalid input. Please enter a valid hostname or URL.")
+                continue
+
+            if len(hostname) <= 4:
+                print("Hostname should be more than 4 characters.")
+                continue
+
+            ip_address = socket.gethostbyname(hostname)
+            store_ip_address(engine, hostname, ip_address)
+            print(f"\n\n{'*' * 40}")
+            print(click.style(f'Hostname: {hostname}', fg='green'))
+            print(f'IP: {ip_address}')
+            print(f"{'*' * 40}\n\n")
+        except socket.gaierror as error:
+            print(click.style(f'Error: Unable to resolve hostname {hostname}.',fg="red"))
