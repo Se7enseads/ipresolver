59b46368815c3a348bf5a8dbb11c4e0e87a56419
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-09-08 00:33:36 +0300

    feat: add display table function and options

--- python/main.py
+++ python/main.py
@@ -9,6 +9,8 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+from tabulate import tabulate
+
 Base = declarative_base()
 engine = create_engine('sqlite:///ip_addresses.db')
 Base.metadata.create_all(engine)
@@ -45,6 +47,21 @@ def get_ip_addresses():
 
     session.close()
     return ip_addresses
+
+
+def display_ip_history():
+    ip_addresses = get_ip_addresses()
+    if ip_addresses:
+        row_data = [(data.hostname, data.ip_address)
+                    for data in ip_addresses]
+        row_id = [data.id for data in ip_addresses]
+        headers = ["Hostname", "IP Address"]
+        print(f"\n\n{'*' * 40}")
+        print(tabulate(row_data, headers, tablefmt="simple_grid", showindex=row_id))
+        print(f"{'*' * 40}\n")
+
+    else:
+        print("No IP addresses in the database.")
 
 
 def resolve_ip():
@@ -84,10 +101,13 @@ def resolve_ip():
 
 @click.command()
 @click.option('--resolve', is_flag=True, help="Resolve and store IP address for a URL.")
-def get_hostname_ip(resolve):
+@click.option('--history', is_flag=True, help="Display the database history.")
+def get_hostname_ip(resolve, history, delete, clear):
 
     if resolve:
         resolve_ip()
+    elif history:
+        display_ip_history()
 
     else:
         questions = [
@@ -95,6 +115,7 @@ def get_hostname_ip(resolve):
                           message="Select an option",
                           choices=[
                               "Resolve and Store IP Address",
+                              "Display IP History",
                               "Exit"
                           ]),
         ]
@@ -104,6 +125,10 @@ def get_hostname_ip(resolve):
 
             if answers['menu'] == "Exit":
                 print("Operation aborted by the user.")
+                break
+
+            elif answers['menu'] == "Display IP History":
+                display_ip_history()
                 break
 
             elif answers['menu'] == "Resolve and Store IP Address":
